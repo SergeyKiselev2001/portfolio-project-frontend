1c507266d05a3ea9d5ed61787473d489c646aaed
Kiselev Sergey <kissergey98@mail.ru>
2023-05-12 04:44:23 +0300

    переход на FSD структуру

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg'
 import './App.css'
-import { MainPage } from './app/MainPage'
+import { MainPage } from './pages/MainPage'
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './config/router/routerConfig'
+import { router } from './app/config/router/routerConfig'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
