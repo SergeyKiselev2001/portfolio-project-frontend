cf11aa6bf8a4486c923b3389d61a3f995dcd1f02
Kiselev Sergey <kissergey98@mail.ru>
2023-05-22 02:41:59 +0300

    обновил структуру конфигурации

--- craco.config.js
+++ craco.config.js
@@ -2,31 +2,47 @@
 const path = require('path')
 const sassResourcesLoader = require('craco-sass-loader')
 
-module.exports = {
-  plugins: [
-    {
-      plugin: sassResourcesLoader,
-      options: {
-        resources: './src/shared/scss/index.scss', // Глобальные стили
-      },
-    },
-    {
-      plugin: {
-        overrideWebpackConfig: ({ webpackConfig }) => {
-          webpackConfig.resolve = {
-            ...webpackConfig.resolve,
-            alias: {
-              ...webpackConfig.resolve.alias,
-              '@app': path.resolve(__dirname, 'src/app'),
-              '@pages': path.resolve(__dirname, 'src/pages'),
-              '@widgets': path.resolve(__dirname, 'src/widgets'),
-              '@shared': path.resolve(__dirname, 'src/shared'),
-            },
-          }
-          return webpackConfig
+const overrideWebpack = {
+  plugin: {
+    overrideWebpackConfig: ({ webpackConfig }) => {
+      webpackConfig.resolve = {
+        ...webpackConfig.resolve,
+        alias: {
+          ...webpackConfig.resolve.alias,
+          '@app': path.resolve(__dirname, 'src/app'),
+          '@pages': path.resolve(__dirname, 'src/pages'),
+          '@widgets': path.resolve(__dirname, 'src/widgets'),
+          '@shared': path.resolve(__dirname, 'src/shared'),
         },
-      },
-      options: {},
+      }
+      return webpackConfig
     },
-  ],
+  },
+  options: {},
 }
+
+const sassLoader = {
+  plugin: sassResourcesLoader,
+  options: {
+    resources: './src/shared/scss/index.scss', // Глобальные стили
+  },
+}
+
+const plugins = [overrideWebpack, sassLoader]
+
+const jest = {
+  configure: (jestConfig) => {
+    return {
+      ...jestConfig,
+      moduleNameMapper: {
+        '^@app/(.+)': '<rootDir>/src/app/$1',
+        '^@pages/(.+)': '<rootDir>/src/pages/$1',
+        '^@widgets/(.+)': '<rootDir>/src/widgets/$1',
+        '^@shared/(.+)': '<rootDir>/src/shared/$1',
+        '\\.(css|scss)$': 'identity-obj-proxy',
+      },
+    }
+  },
+}
+
+module.exports = { plugins, jest }
