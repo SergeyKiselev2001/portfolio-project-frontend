2e1dd1d8ae89385cb4d6eeee0bc73e8770a08259
Kiselev Sergey <kissergey98@mail.ru>
2023-06-16 02:22:45 +0300

    скелетон для aside

--- src/widgets/BlockSideInfo/ui/BlockSideInfo.tsx
+++ src/widgets/BlockSideInfo/ui/BlockSideInfo.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react'
 import classes from './BlockSideInfo.module.scss'
+import { StorageKeys, getStorageItem } from '@entities/clientStorage'
+import { BlockSideSkeleton } from './BlockSideSkeleton'
 
 const BlockSideInfo = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false)
+
+  useEffect(() => {
+    if (getStorageItem(StorageKeys.AUTH)) {
+      setIsAuthorized(true)
+    }
+  }, [])
+
   return (
     <div className={classes.BlockSideInfo}>
-      <div className={classes.mainInfo}>Main</div>
-      <div className={classes.subscribers}>10 Подписчиков</div>
-      <div className={classes.links}>ссылки</div>
-      <button>Добавить пост</button>
+      {!isAuthorized && <BlockSideSkeleton />}
+      {isAuthorized && (
+        <>
+          <div className={classes.mainInfo}>Main</div>
+          <div className={classes.subscribers}>10 Подписчиков</div>
+          <div className={classes.links}>ссылки</div>
+          <button>Добавить пост</button>
+        </>
+      )}
     </div>
   )
 }
---
+++ src/widgets/BlockSideInfo/ui/BlockSideSkeleton.tsx
@@ -0,0 +1,9 @@
+import classes from './BlockSideInfo.module.scss'
+
+export const BlockSideSkeleton = () => {
+  return (
+    <div className={classes.BlockSideSkeleton}>
+      <div className={classes.light} />
+    </div>
+  )
+}
